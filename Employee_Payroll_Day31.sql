223ca67b8b14ac29fdbfe732797b778275ac0fe5
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-24 22:33:10 +0530

    [Tushar Vidwans] Creating employee_payroll table and adding require fields

--- Employee_Payroll_Day31.sql
+++ Employee_Payroll_Day31.sql
@@ -3,3 +3,15 @@
 /*UC1 - Creating payroll service database*/
 
 create database payroll_service
+
+/*UC2 - Creating employee_payroll table and adding fields*/
+
+create table employee_payroll
+(
+Id int PRIMARY KEY identity,
+Name varchar(30),
+Salary int,
+StartDate date
+)
+
+drop table employee_payroll

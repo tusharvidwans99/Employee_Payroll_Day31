935bfa09850ccd5b5b97c202f9df93a3c52f461c
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-27 22:36:22 +0530

    [Tushar Vidwans] Adding columns like phone_number, address and department

---
+++ Employee_Payroll_Day31.sql
@@ -0,0 +1,78 @@
+/*Welcome to the Employee payroll SQL program*/
+
+/*UC1 - Creating payroll service database*/
+
+create database payroll_service
+
+/*UC2 - Creating employee_payroll table and adding fields*/
+
+create table employee_payroll
+(
+Id int PRIMARY KEY identity,
+Name varchar(30),
+Salary int,
+StartDate date
+)
+
+drop table employee_payroll
+
+
+/*UC3 - Adding data into the table*/
+
+insert into employee_payroll(Name,Salary,StartDate) values('Suresh', 1000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Tushar', 2000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Amit', 3000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Chaitra',4000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Ajay', 5000, GETDATE())
+
+/*UC4 - Retrieving the data which was inserted into the employee_peyroll table*/ 
+select * from employee_payroll
+
+/*UC5 - Retrieving the salary data for speicific person and for all employees which com in speicif date*/
+select Salary from employee_payroll
+where Name='Amit'
+
+select salary from employee_payroll
+where StartDate between cast('2022-06-20' as date) and GETDATE()
+
+/*UC6 - Adding Gender column in employee_table to show the gender of employee*/
+alter table employee_payroll
+Add Gender char
+
+update employee_payroll
+set Gender='M'
+where Id in (1,2,3,5)
+
+update employee_payroll
+set Gender='F'
+where Id=4
+
+select * from employee_payroll
+
+
+/*UC7 - Calculating sum, avg, min, max of salary and count of Female and Male*/
+/*SUM, AVG, MIN, MAX, COUNT*/
+select sum(Salary),Gender from employee_payroll
+GROUP BY Gender
+
+
+select avg(Salary),Gender from employee_payroll
+GROUP BY Gender
+
+select min(Salary),Gender from employee_payroll
+GROUP BY Gender
+
+select max(Salary),Gender from employee_payroll
+GROUP BY Gender
+
+select count(Salary),Gender from employee_payroll
+GROUP BY Gender
+
+/*UC8 - extending payroll_service table*/
+
+select * from employee_payroll
+
+alter table employee_payroll add phone_number varchar(250)
+alter table employee_payroll add address varchar(250)
+alter table employee_payroll add department varchar(150) not null default '0'
+insert into employee_payroll (name, salary, StartDate) values('Bill',100000.0,'2018-01-03')

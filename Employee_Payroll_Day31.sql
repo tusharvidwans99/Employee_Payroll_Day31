56160f1c84e5a20d29286848dae4a425d13cc01c
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-28 00:26:43 +0530

    [Tushar Vidwans] Addingle new columns like deductions, taxable_pay,tax, net_pay

--- Employee_Payroll_Day31.sql
+++ Employee_Payroll_Day31.sql
@@ -68,7 +68,7 @@ GROUP BY Gender
 select count(Salary),Gender from employee_payroll
 GROUP BY Gender
 
-/*UC8 - extending payroll_service table*/
+/*UC8 - extending payroll_service table by adding columns like phone_number, address, department*/
 
 select * from employee_payroll
 
@@ -76,3 +76,22 @@ alter table employee_payroll add phone_number varchar(250)
 alter table employee_payroll add address varchar(250)
 alter table employee_payroll add department varchar(150) not null default '0'
 insert into employee_payroll (name, salary, StartDate) values('Bill',100000.0,'2018-01-03')
+
+
+/* UC9 - extending payroll_service table by adding columns like deduction, taxable_pay, tax, net_pay*/
+
+exec sp_rename 'employee_payroll.Salary' , 'basic_pay'  /*change name of the column*/
+alter table employee_payroll add deductions float
+alter table employee_payroll add taxable_pay float
+alter table employee_payroll add tax float
+alter table employee_payroll add net_pay float
+
+update employee_payroll
+set department='Sales'
+where Name='Bill'
+
+select * from employee_payroll
+
+
+insert into employee_payroll(Name,department,Gender,basic_pay,deductions,taxable_pay,tax,net_pay,StartDate)
+values('Terisa', 'IT','F',3000000.00,100000.00,2000000.00,500000.00,1500000.00,GETDATE())
